3ed39a90ca9443da60dd6e0dd82f09488bcd4989
Anbu Sivam.B <anbulegend101@gmail.com>
2025-07-30 17:56:36 +0530

    Header.tsx

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -52,8 +52,8 @@ export function Header() {
                 data-testid="header-logo"
               />
             </Link>
-            <motion.h2
-  className="ml-3 text-xl md:text-3xl lg:text-5xl xl:text-3xl font-bold leading-tight text-red-500 whitespace-nowrap truncate"
+           <motion.h2
+  className="ml-3 text-2xl font-bold leading-tight text-red-500 whitespace-nowrap truncate"
   data-testid="company-name"
   initial={{ opacity: 0 }}
   animate={{ opacity: 1 }}
@@ -61,6 +61,7 @@ export function Header() {
 >
   VLGE INSTITUTE PVT LTD
 </motion.h2>
+
           </motion.div>
 
           {/* Navigation (Desktop) */}
